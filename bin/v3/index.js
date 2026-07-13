752bdbd193b60d5fff2cfb20cd30fd39de5fb60d
keshavsoft <nknnkumar@yahoo.com>
2026-07-13 18:56:33 +0530

    3

---
+++ bin/v3/index.js
@@ -0,0 +1,16 @@
+import { getAllImports } from './getAllImports.js';
+import { getImportCount } from './getImportCount.js';
+import { isImportPresent } from './isImportPresent.js';
+import { getImportStartLine } from './getImportStartLine.js';
+import { getImportEndLine } from './getImportEndLine.js';
+import { getImportVariables } from './getImportVariables.js';
+
+export {
+    getAllImports,
+    getImportCount,
+    isImportPresent,
+    getImportStartLine,
+    getImportEndLine,
+    getImportVariables
+};
+
